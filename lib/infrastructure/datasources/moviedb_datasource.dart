617f7ec1679f24b95f7342fe015ed293553cb9a6
Seom1177 <cesarleonardo88@hotmail.com>
2023-12-07 17:02:18 -0500

    use mapper on moviedb_datasource

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -2,6 +2,8 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasources.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDataSource {
@@ -14,7 +16,16 @@ class MoviedbDatasource extends MoviesDataSource {
   @override
   Future<List<Movie>> getnowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
-    return [];
+
+    // moviedb respond to object (MovieDbResponse)
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    // MovieDbResponse object to movie entity
+    final List<Movie> movies = movieDbResponse.results
+        .where((moviedb) => moviedb.posterPath != 'Not-Found') // dont render a movie without poster
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
+
+    return movies;
   }
 }
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -15,7 +15,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'https://linnea.com.ar/wp-content/uploads/2018/09/404PosterNotFound.jpg',
+          : 'Not-Found',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
