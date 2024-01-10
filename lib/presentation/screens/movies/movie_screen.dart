05e12159c50b3a804739f6141e85193adfe6bba8
Seom1177 <cesarleonardo88@hotmail.com>
2024-01-10 14:56:40 -0500

    movie Detail - Caché local

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   const MovieScreen({super.key, required this.movieId});
 
   static const name = 'movie-screen';
   final String movieId;
 
   @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+    
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MovieId $movieId'),
+        title: Text('MovieId ${widget.movieId}'),
       ),
     );
   }
