ec2ece410e69a1296573cb73c033c71d8ad19d41
Seom1177 <cesarleonardo88@hotmail.com>
2024-01-10 13:52:19 -0500

    MoviesSlideshow: removing unused parameter

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -31,7 +31,7 @@ class MoviesSlideshow extends StatelessWidget {
 }
 
 class _Slide extends StatelessWidget {
-  const _Slide({super.key, required this.movie});
+  const _Slide({required this.movie});
   final Movie movie;
 
   @override
