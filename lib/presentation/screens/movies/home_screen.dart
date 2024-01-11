5743ca874e5c71b2ed3593e4f56454e769874c2e
Seom1177 <cesarleonardo88@hotmail.com>
2024-01-11 15:50:00 -0500

    Funcionamiento del bottom navigation bar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,17 +1,27 @@
+import 'package:cinemapedia/presentation/views/movies/favorites_view.dart';
 import 'package:cinemapedia/presentation/views/movies/home_view.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.pageIndex});
+  final int pageIndex;
   static const name = 'home-screen';
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(),
+    FavoritesView()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
+      bottomNavigationBar: CustomBottomNavigation(currenIndex: pageIndex,),
     );
   }
 }
-
