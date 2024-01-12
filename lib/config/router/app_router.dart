59c79d6bab3470893b4b727b2d54b8fc99bbcb1f
Seom1177 <cesarleonardo88@hotmail.com>
2024-01-11 21:28:49 -0500

    State-Preserving Go-Router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,33 +3,67 @@ import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  ShellRoute(
-      builder: (context, state, child) {
-        return HomeScreen(childView: child);
-      },
-      routes: [
-        GoRoute(
+  // State-Preserving
+  StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) =>
+          HomeScreen(childView: navigationShell),
+      branches: [
+        StatefulShellBranch(routes: <RouteBase>[
+          GoRoute(
             path: '/',
             builder: (context, state) {
               return const HomeView();
             },
             routes: [
               GoRoute(
-                path: 'movie/:id',
-                name: MovieScreen.name,
-                builder: (context, state) {
-                  final movieId = state.pathParameters['id'] ?? 'no-id';
-                  return MovieScreen(movieId: movieId);
-                },
-              ),
-            ]),
-        GoRoute(
-          path: '/favorites',
-          builder: (context, state) {
-            return const FavoritesView();
-          },
-        )
-      ])
+                  path: 'movie/:id',
+                  name: MovieScreen.name,
+                  builder: (context, state) {
+                    final movieId = state.pathParameters['id'] ?? 'no-id';
+                    return MovieScreen(movieId: movieId);
+                  },
+                ),
+            ]
+          )
+        ]),
+        StatefulShellBranch(routes: <RouteBase>[
+          GoRoute(
+            path: '/favorites',
+            builder: (context, state) {
+              return const FavoritesView();
+            },
+          )
+        ]),
+      ]),
+
+  // non State-Preserving
+  // ShellRoute(
+  //     builder: (context, state, child) {
+  //       return HomeScreen(childView: child);
+  //     },
+  //     routes: [
+  //       GoRoute(
+  //           path: '/',
+  //           builder: (context, state) {
+  //             return const HomeView();
+  //           },
+  //           routes: [
+  //             GoRoute(
+  //               path: 'movie/:id',
+  //               name: MovieScreen.name,
+  //               builder: (context, state) {
+  //                 final movieId = state.pathParameters['id'] ?? 'no-id';
+  //                 return MovieScreen(movieId: movieId);
+  //               },
+  //             ),
+  //           ]),
+  //       GoRoute(
+  //         path: '/favorites',
+  //         builder: (context, state) {
+  //           return const FavoritesView();
+  //         },
+  //       )
+  //     ])
 
   // Rutas padre/hijo
   // GoRoute(
